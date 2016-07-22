6c001fe93b81afcc200fe3a7cc2bdfd6311e36f9
canglingyue <canglingyue@126.com>
2016-07-22 13:13:46 +0800

    加入markdown

--- ByBlog/Admin/Controller/BlogController.class.php
+++ ByBlog/Admin/Controller/BlogController.class.php
@@ -8,8 +8,21 @@
 namespace Admin\Controller;
 use Think\Controller;
 class BlogController extends Controller {
+    public function index(){
+        $this -> display();
+    }
     public function add(){
-        
+        $Form   =   D('Form');
+        if($Form->create()) {
+            $result =   $Form->add();
+            if($result) {
+                $this->success('数据添加成功！');
+            }else{
+                $this->error('数据添加错误！');
+            }
+        }else{
+            $this->error($Form->getError());
+        }
     }
 
 }
