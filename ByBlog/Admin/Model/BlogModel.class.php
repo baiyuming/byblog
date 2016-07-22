e051f67f079ae9ba2df0b83f037e2f56ff14215c
canglingyue <canglingyue@126.com>
2016-07-22 16:52:33 +0800

    前端 更换 表

--- ByBlog/Admin/Model/BlogModel.class.php
+++ ByBlog/Admin/Model/BlogModel.class.php
@@ -13,10 +13,10 @@ use Think\Model;
 class BlogModel extends Model {
     // 定义自动验证
     protected $_validate    =   array(
-        array('title','require','标题必须'),
+        array('a_title','require','标题必须'),
     );
     // 定义自动完成
     protected $_auto    =   array(
-        array('create_time','time',1,'function'),
+        array('a_time','time',1,'function'),
     );
 }
