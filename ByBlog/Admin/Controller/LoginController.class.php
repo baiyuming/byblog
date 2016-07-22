749e49a7cfa1d75b1a4d2c4d91968ba956df5a0c
canglingyue <canglingyue@126.com>
2016-07-22 20:26:50 +0800

    后台 登录

--- ByBlog/Admin/Controller/LoginController.class.php
+++ ByBlog/Admin/Controller/LoginController.class.php
@@ -2,23 +2,50 @@
 namespace Admin\Controller;
 use Think\Controller;
 class LoginController extends Controller {
+    /**
+     * 用户登录
+     */
     public function login(){
-        $this -> display();
-    }
-    public function loginSucc(){
-        $User   =   M('User');
-        $userName = I('post.userName');
-        $pwd = I('post.pwd');
-        $condition['userName'] = $userName;
-        $date = $User -> field('pwd') ->where($condition)->select();
-
-        if($pwd == $date[0]['pwd']){
-            echo '1111';
-        }else{
-            echo '22222';
+        // 判断提交方式
+        if (IS_POST) {
+            // 实例化Login对象
+            $login = D('login');
+            // 自动验证 创建数据集
+            if (!$data = $login->create()) {
+                // 防止输出中文乱码
+                header("Content-type: text/html; charset=utf-8");
+                exit($login->getError());
+            }
+            // 组合查询条件
+            $where = array();
+            $where['username'] = $data['username'];
+            $result = $login->where($where)->field('userid,username,nickname,password,lastdate,lastip')->find();
+            // 验证用户名 对比 密码
+            if ($result && $result['password'] == $result['password']) {
+                // 存储session
+                session('uid', $result['userid']);          // 当前用户id
+                session('nickname', $result['nickname']);   // 当前用户昵称
+                session('username', $result['username']);   // 当前用户名
+                session('lastdate', $result['lastdate']);   // 上一次登录时间
+                session('lastip', $result['lastip']);       // 上一次登录ip
+                // 更新用户登录信息
+                $where['userid'] = session('uid');
+                M('users')->where($where)->setInc('loginnum');   // 登录次数加 1
+                M('users')->where($where)->save($data);   // 更新登录时间和登录ip
+                $this->success('登录成功,正跳转至系统首页...', U('Index/index'));
+            } else {
+                $this->error('登录失败,用户名或密码不正确!');
+            }
+        } else {
+            $this->display();
         }
-
-        //$this -> assgin('post',$_POST);
-        $this->display();
+    }
+    /**
+     * 用户注销
+     */
+    public function logout(){
+        // 清楚所有session
+        session(null);
+        redirect(U('Login/login'), 2, '正在退出登录...');
     }
 }
