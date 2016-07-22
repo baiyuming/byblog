42ca0094d82460056d0b9404499b2688969e2d21
canglingyue <canglingyue@126.com>
2016-07-22 21:36:02 +0800

    后台 登录

--- ByBlog/Admin/Controller/LoginController.class.php
+++ ByBlog/Admin/Controller/LoginController.class.php
@@ -1,51 +1,24 @@
 <?php
-namespace Admin\Controller;
+namespace User\Controller;
 use Think\Controller;
 class LoginController extends Controller {
-    /**
-     * 用户登录
-     */
-    public function login(){
-        // 判断提交方式
-        if (IS_POST) {
-            // 实例化Login对象
-            $login = D('login');
-            // 自动验证 创建数据集
-            if (!$data = $login->create()) {
-                // 防止输出中文乱码
-                header("Content-type: text/html; charset=utf-8");
-                exit($login->getError());
-            }
-            // 组合查询条件
-            $where = array();
-            $where['username'] = $data['username'];
-            $result = $login->where($where)->field('userid,username,nickname,password,lastdate,lastip')->find();
-            // 验证用户名 对比 密码
-            if ($result && $result['password'] == $result['password']) {
-                // 存储session
-                session('uid', $result['userid']);          // 当前用户id
-                session('nickname', $result['nickname']);   // 当前用户昵称
-                session('username', $result['username']);   // 当前用户名
-                session('lastdate', $result['lastdate']);   // 上一次登录时间
-                session('lastip', $result['lastip']);       // 上一次登录ip
-                // 更新用户登录信息
-                $where['userid'] = session('uid');
-                M('users')->where($where)->setInc('loginnum');   // 登录次数加 1
-                M('users')->where($where)->save($data);   // 更新登录时间和登录ip
-                $this->success('登录成功,正跳转至系统首页...', U('Index/index'));
-            } else {
-                $this->error('登录失败,用户名或密码不正确!');
-            }
-        } else {
-            $this->display();
-        }
+    // 登录控制器
+    public function index(){
+        // 登陆模板
+        $this->display();
     }
-    /**
-     * 用户注销
-     */
-    public function logout(){
-        // 清楚所有session
-        session(null);
-        redirect(U('Login/login'), 2, '正在退出登录...');
+    public function login(){
+        // 登陆用户名提交判断
+        $name = I('post.name');
+        $password = I('post.password');
+        $row = D('User')->login($name,$password);
+        // 用D方法在数据库进行验证后返回
+        if($row == 1){
+            $this->redirect('User/Index/index');
+        }elseif($row == 0){
+            jumpError('用户名和密码不能为空！请重新输入！');
+        }else{
+            jumpError('用户名或者密码错误，请重新输入');
+        }
     }
 }
---
+++ ByBlog/Admin/Model/LoginModel.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BYM
+ * Date: 2016/7/22
+ * Time: 21:15
+ */
+
+namespace User\Model;
+use Think\Model;
+class UserModel extends Model{
+######################################用户操作数据模型######################################
+    public function login($name, $password){
+        // 登陆验证
+        if ($name == '' || $password == '') {
+            return 0;
+        }
+        $login = M('user')->where(array('name' => $name))->find();
+        if ($login['password'] == $password) {
+            // 验证通过 更新登陆时候的IP和时间
+            $data = array(
+                'last_time' => time(),
+                'ip' => get_client_ip());
+            // 更新数据库文件
+            M('user')->where(array('name' => $name))->save($data);
+            // 验证文件写入SESSION;
+            SESSION('uid', $login['id']);
+            SESSION('user', $login['user']);
+            SESSION('name', $login['name']);
+            SESSION('class', $login['class']);
+            SESSION('last_time', date('Y-m-d H:i:s', $login['last_time']));
+            SESSION('ip', $login['loginip']);
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
