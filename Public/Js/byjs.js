554c9defcad4ae6d43b01bfdbccb8768655f3289
canglingyue <canglingyue@126.com>
2016-07-29 15:52:19 +0800

    添加百度统计代码

--- Public/Js/byjs.js
+++ Public/Js/byjs.js
@@ -5,3 +5,27 @@
 // 初始化导航信息
 
 $(".button-collapse").sideNav();
+// weber.pub
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?21e84bab8a9685ddee15fda30a812860";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+// vuejs.pub
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?a6e211e094a3c37f3cc335193a574802";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+// babyli.wang
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?a12aa47962b81f71c9f0d5aea07bef82";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
