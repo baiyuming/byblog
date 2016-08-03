c3908fd8608fe975d35667d3382d6890abfea999
canglingyue <canglingyue@126.com>
2016-08-03 09:56:33 +0800

    新增文章点击次数统计

--- ByBlog/Home/Controller/ContentController.class.php
+++ ByBlog/Home/Controller/ContentController.class.php
@@ -3,11 +3,17 @@ namespace Home\Controller;
 use Think\Controller;
 class ContentController extends Controller {
     public function content($id=2){
-        $Form   =   M('Article');
+        $Article   =   M('Article');
         // 读取数据
-        $data =   $Form->find($id);
+        $data =   $Article->find($id);
         //var_dump($data);
         if($data) {
+            $hit = array(
+                'a_id' => $id,
+                'a_hit' => $data['a_hit'] + 1
+            );
+            // 更新文章点击次数
+            $Article -> save($hit);
             $this->assign('data',$data);// 模板变量赋值
         }else{
             $this->error('数据错误');
